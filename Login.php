0a6e56dc6d9f60923d541d4f8469d70fd25fc194
Anushka Rangari <83566191+ar7937@users.noreply.github.com>
2021-11-17 12:22:04 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -528,7 +528,39 @@ input[type=text]{
 
 
         <div class="section">
-            
+            <div class="section">
+            <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="vaccine.jpg" style="width:100%">
+  <div class="text">Vaccine</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="doctor.jpg" style="width:100%">
+  <div class="text">Doctor</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="state1.jpg" style="width:100%">
+  <div class="text">State</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+        </div>
         </div>
         
          <div class="article">
